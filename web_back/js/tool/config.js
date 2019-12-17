c3fb725f3518dd9eb53f0fe3fc0876fb6b54d7aa
your dad <htguangbo@sina.com>
2019-12-17 12:23:16 +0800

    用户信息修改

--- web_back/js/tool/config.js
+++ web_back/js/tool/config.js
@@ -1,4 +1,4 @@
 var BASE_URL = "http://localhost:8000/";
 var LOGIN = BASE_URL + "admin/login";
 var USER = BASE_URL + "admin/logout";
-var USER_INFO = BASE_URL + "article_count";
+var USER_INFO = BASE_URL + "admin/userinfo_get";
