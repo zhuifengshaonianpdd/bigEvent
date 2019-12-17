b517ab04b4e476228a5f9c65de0d80cedcebfe08
your dad <htguangbo@sina.com>
2019-12-17 20:51:01 +0800

    封装分块性能完成

--- web_back/js/tool/config.js
+++ web_back/js/tool/config.js
@@ -2,3 +2,7 @@ var BASE_URL = "http://localhost:8000/";
 var LOGIN = BASE_URL + "admin/login";
 var USER = BASE_URL + "admin/logout";
 var USER_INFO = BASE_URL + "admin/userinfo_get";
+var INFO_Show = BASE_URL + "admin/category_search";
+var INFO_Add = BASE_URL + "admin/category_add";
+var INFO_Edit = BASE_URL + "admin/category_edit";
+var INFO_Del = BASE_URL + "admin/category_delete";
--- web_back/js/tool/user.js
+++ web_back/js/tool/user.js
@@ -42,5 +42,56 @@ var user = {
             }
         })
 
+    },
+    infoShow: function (options) {
+        $.ajax({
+            type: "get",
+            url: INFO_Show,
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success(res)
+                }
+            }
+        })
+    },
+    infoAdd: function (options) {
+        $.ajax({
+            type: "post",
+            url: INFO_Add,
+            data: options.data,
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success()
+                } else {
+                    options.fail()
+                }
+            }
+        })
+    },
+    infoEdit: function (options) {
+        $.ajax({
+            type: "post",
+            url: INFO_Edit,
+            data: data,
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success()
+                } else {
+                    options.fail()
+                }
+            }
+        })
+    },
+    infoDel: function (options) {
+        $.ajax({
+            type: "post",
+            url: INFO_Del,
+            data: options.data,
+            success: function (res) {
+                if (res.code === 200) {
+                    options.success()
+                }
+            }
+        })
     }
 }
